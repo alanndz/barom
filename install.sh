141044ebac1c62d24238ea5c28fcf0fde1e8fccd
alanndz <alanndz@qmail.id>
2021-05-29 15:15:48 +0000

    install: update gdrive
    Signed-off-by: alanndz <alanndz@qmail.id>

--- install.sh
+++ install.sh
@@ -11,8 +11,11 @@ wget -O /tmp/barom https://git.io/JUjwP
 chmod +x /tmp/barom
 sudo install /tmp/barom /usr/local/bin/barom
 
-wget https://raw.githubusercontent.com/usmanmughalji/gdriveupload/master/gdrive
+wget https://github.com/AnggaR96s/gdrive/releases/download/2.2.0/gdrive_2.2.0_linux_386.tar.gz
+tar xf gdrive_2.2.0_linux_386.tar.gz
 chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive 
+sudo install gdrive /usr/local/bin/gdrive
+rm gdrive
+rm gdrive_2.2.0_linux_386.tar.gz
 
 
