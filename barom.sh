9eadf9263e9a7fb34a34d71641743c8210366234
alanndz <alanndz@qmail.id>
2021-09-26 23:30:52 +0000

    barom: Specific rom every build-result
    Signed-off-by: alanndz <alanndz@qmail.id>

--- barom.sh
+++ barom.sh
@@ -92,7 +92,7 @@ setup_env() {
 	SF_USER="$(dnc sf_user)"
 	SF_PW="$(dnc sf_pw)"
 	O="out/target/product/$DEVICE"
-	RESULT="../build-result"
+	RESULT="../build-result-${LUNCH}"
 }
 ############## END ##############
 # Reseting all configure
