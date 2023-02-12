b456a93d028b28aecc41d3d5f009953e2ea34ec4
alanndz <alanndz7@gmail.com>
2023-02-12 18:16:55 +0800

    install: Test
    Signed-off-by: alanndz <alanndz7@gmail.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ -n $1 ]]
+if [[ -n "$1" ]]
 then
-	BRANCH=$1
+	BRANCH="$1"
 else
 	BRANCH=main
 fi
 
-curl -Lo /tmp/barom https://github.com/alanndz/barom/raw/$BRANCH/barom.sh # https://git.io/JUjwP
+curl -Lo /tmp/barom https://raw.githubusercontent.com/alanndz/barom/"$BRANCH"/barom.sh # https://git.io/JUjwP
 chmod +x /tmp/barom
 sudo install /tmp/barom /usr/local/bin/barom
